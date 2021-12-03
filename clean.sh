b98d4c0482062e76ae49da58e015d6a29283a4c8
JieMin <jiemind@outlook.com>
2021-12-03 20:45:15 +0800

    add del PG

--- clean.sh
+++ clean.sh
@@ -2,6 +2,7 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 echo $SHELL_FOLDER
 sudo rm -rf $SHELL_FOLDER/BACKEND/project/temp
 sudo rm -rf $SHELL_FOLDER/BACKEND/project/data-test.db
+sudo rm -rf $SHELL_FOLDER/BACKEND/data/postgresql
 sudo rm -rf $SHELL_FOLDER/ES/es01/data
 sudo rm -rf $SHELL_FOLDER/ES/es01/logs
 sudo rm -rf $SHELL_FOLDER/ES/es02/data
