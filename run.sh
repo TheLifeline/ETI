0c41850f3fc13c5fa5fc1e2178f2cbb7599e9eed
JieMin <jiemind@outlook.com>
2021-12-08 15:29:23 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ sudo docker-compose -f $SHELL_FOLDER/BACKEND/docker-compose.dev.yml up -d
 if ifconfig |grep eth0 >/dev/null ;then
 echo window.axiosBaseUrl=\"http://`ip addr|grep inet|grep eth|awk '{print $2}'|awk -F '/' '{print $1}'`:5000/\" > $SHELL_FOLDER/FRONTEND/public/config.js;                        
 else
-echo window.axiosBaseUrl=\"http://`ip addr|grep inet|grep ens|awk '{print $2}'|awk -F '/' '{print $1}'`:5000/\" > $SHELL_FOLDER/FRONTEND/public/config.js;                     
+echo window.axiosBaseUrl=\"http://`ip addr|grep inet|grep ens33|awk '{print $2}'|awk -F '/' '{print $1}'`:5000/\" > $SHELL_FOLDER/FRONTEND/public/config.js;                     
 fi
 cd $SHELL_FOLDER/FRONTEND
-npm install && npm run build
+npm install
+npm run build
 sudo docker-compose -f $SHELL_FOLDER/FRONTEND/docker-compose.dev.yml up -d
